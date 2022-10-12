c0d3c72fe7179939b98e3875e2dc1be0f7290095
AceAtDev <aceatdeveloping@gmail.com>
2022-10-11 22:09:08 -0400

    Finished Building the basic Rooms graphics

--- Game/Rooms/Templates.java
+++ Game/Rooms/Templates.java
@@ -79,42 +79,208 @@ public interface Templates {
    }
 
    public static void oneGateUp(){
-      System.out.println
-         ("+-------    -------+\n"+
-         "+                  +\n"+
-         "+                  +\n"+
-         "+                  +\n"+
-         "+                  +\n"+      
-         "+                  +\n"+
-         "+                  +\n"+
-          "+------------------+");
+      System.out.println("+---------+          +--------+");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         +----------+        |");
+      System.out.println("|                             |");
+      System.out.println("+-----------------------------+");
    }
 
    public static void oneGateDown(){
-      System.out.println
-         ("+------------------+\n"+
-         "+                  +\n"+
-         "+                  +\n"+
-         "+                  +\n"+
-         "+                  +\n"+      
-         "+                  +\n"+
-         "+                  +\n"+
-         "+-------    -------+");
+      System.out.println("+-----------------------------+");
+      System.out.println("|                             |");
+      System.out.println("|         +----------+        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("+---------+          +--------+");
    }
 
 
 
 
 
-   static void twoGatesUpDown(){
 
-      System.out.println("+-------    -------+");
-      System.out.println("+                  +");
-      System.out.println("+                  +");
-      System.out.println("+                  +");
-      System.out.println("+                  +");
-      System.out.println("+                  +");
-      System.out.println("+-------    -------+");
 
+
+   public static void twoGatesUpDown(){
+
+      System.out.println("+---------+          +--------+");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("+---------+          +--------+");
+   }
+
+
+   public static void twoGatesRightLeft(){
+      System.out.println("+-----------------------------+");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("+-----------------------------+");
+      System.out.println("                               ");
+      System.out.println("                               ");
+      System.out.println("                               ");
+      System.out.println("+-----------------------------+");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("+-----------------------------+");
+   }
+
+   public static void twoGatesUpLeft(){
+      System.out.println("+---------+          +--------+");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("+---------+          |        |");
+      System.out.println("                     |        |");
+      System.out.println("                     |        |");
+      System.out.println("                     |        |");
+      System.out.println("+--------------------+        |");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("+-----------------------------+");
+   }
+
+   public static void twoGatesLeftDown(){
+      System.out.println("+-----------------------------+");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("+--------------------+        |");
+      System.out.println("                     |        |");
+      System.out.println("                     |        |");
+      System.out.println("                     |        |");
+      System.out.println("+---------+          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("+---------+          +--------+");
+   }
+
+   public static void twoGatesUpRight(){
+      System.out.println("+---------+          +--------+");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          +--------+");
+      System.out.println("|         |                    ");
+      System.out.println("|         |                    ");
+      System.out.println("|         |                    ");
+      System.out.println("|         +-------------------+");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("+-----------------------------+");
+   }
+
+   public static void twoGatesRightDown(){
+      System.out.println("+-----------------------------+");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("|         +-------------------+");
+      System.out.println("|         |                    ");
+      System.out.println("|         |                    ");
+      System.out.println("|         |                    ");
+      System.out.println("|         |          +--------+");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("+---------+          +--------+");
+   }
+
+   
+
+
+
+
+   public static void threeGatesDownRightLeft(){
+      System.out.println("+-----------------------------+");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("+-----------------------------+");
+      System.out.println("                               ");
+      System.out.println("                               ");
+      System.out.println("                               ");
+      System.out.println("+---------+          +--------+");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("+---------+          +--------+");
+   }
+
+   public static void threeGatesRightDownUp(){
+      System.out.println("+---------+          +--------+");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          +--------+");
+      System.out.println("|         |                    ");
+      System.out.println("|         |                    ");
+      System.out.println("|         |                    ");
+      System.out.println("|         |          +--------+");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("+---------+          +--------+");
+   }
+
+   public static void threeGatesLeftUpDown(){
+      System.out.println("+---------+          +--------+");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("+---------+          |        |");
+      System.out.println("                     |         ");
+      System.out.println("                     |         ");
+      System.out.println("                     |         ");
+      System.out.println("+---------+          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("+---------+          +--------+");
+   }
+
+   public static void threeGatesLeftRightUp(){
+      System.out.println("+---------+          +--------+");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("|         |          |        |");
+      System.out.println("+---------+          +--------+");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("+-----------------------------+");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("|                             |");
+      System.out.println("+-----------------------------+");
    }
 }
