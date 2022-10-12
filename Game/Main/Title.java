43ffb2e7395917fd9a7870d74216c6723b0685c5
AceAtDev <aceatdeveloping@gmail.com>
2022-10-11 20:40:42 -0400

    Added Game Title

--- Game/Main/Title.java
+++ Game/Main/Title.java
@@ -1,5 +1,12 @@
 package Game.Main;
 
+import External.Tools;
+
 final class Title {
    
+   public static void draw(){
+      System.out.println("Mr P's Game");
+      Tools.AskString("Press ENTER to continue");
+   }
+
 }
