3de9aed0ec6c548a226969c2f3a2748d9a0e9b72
AceAtDev <aceatdeveloping@gmail.com>
2022-10-19 12:22:01 -0400

    Improved losing screen

--- Game/Main/LoseScreen.java
+++ Game/Main/LoseScreen.java
@@ -14,15 +14,18 @@ public class LoseScreen {
       SoundManager.playSE(3);
       Random rand = new Random();
 
-      for (int i = 0; i < 330; i++){
+      for (int i = 0; i < 340; i++){
          int losingEffect = rand.nextInt(999999999);
+         int losingEffect2 = rand.nextInt(999999);
          Tools.delayer(20);
-         System.out.println(losingEffect + "" + losingEffect + "" + losingEffect + "" + losingEffect + "" + losingEffect);
+         System.out.println(losingEffect + "" + losingEffect2 + "" + losingEffect + "" + losingEffect2 + "" + losingEffect
+         + "" + losingEffect2+ "" + losingEffect);
       }
 
+      
       Tools.delayer(1500);
       System.out.println("YOU DIED");
-      System.out.println("AND YOU HAVE LOST YOUR CHANCE IN FINDING WEALTH");
+      System.out.println("AND YOU HAVE LOST YOUR CHANCE IN FINDING WEALTH, OR DID YOU?");
 
       System.exit(0);
 
