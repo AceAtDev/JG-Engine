bd784ed9624b59c867a18c97008fd0abcd077fa3
AceAtDev <aceatdeveloping@gmail.com>
2022-10-25 03:27:10 -0400

    Added cutscens

--- Game/Levels/Level1.java
+++ Game/Levels/Level1.java
@@ -1,10 +1,13 @@
 package Game.Levels;
 
 import Game.Gameplay.Controls.PlayerController;
+import Game.Gameplay.Cutscenes.Cutscene;
 
 public class Level1 extends Hierarchy{
    
    public void start(){
+
+      Cutscene.startingCutscene();
 
       PlayerController pc = new PlayerController(currentFloor, startingRoom);
       pc.start();
