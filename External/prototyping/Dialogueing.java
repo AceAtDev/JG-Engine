e78f529c61c2712a62d766799d66dbd13dd2e6c0
AceAtDev <aceatdeveloping@gmail.com>
2022-11-04 13:58:30 -0400

    [re-upload]

--- External/prototyping/Dialogueing.java
+++ External/prototyping/Dialogueing.java
@@ -9,11 +9,13 @@ import Game.Gameplay.audio.SoundManager;
 
 class Prototyping{
 
+   /*
    public static void main(String[] args) {
       example("this is a conversion that is happing right now that it should get animated and make sure on the sound because it is kninda importent");
       Tools.delayer(1000);
       example("this is also some text to check on because testing things is fun as wow");
    }
+   */
 
    static void example(String sentences) {
       char[] senChar = sentences.toCharArray();
